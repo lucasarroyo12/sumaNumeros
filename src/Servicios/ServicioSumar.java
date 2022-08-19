54300f1aea25e72897af28b4f07ddee3571a68e3
Lucas Arroyo <lucasarroyo12@gmail.com>
2022-08-18 22:22:20 -0300

    proyecto suma de numeros

---
+++ src/Servicios/ServicioSumar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servicios;
+
+/**
+ *
+ * @author Lucas
+ */
+public class ServicioSumar {
+    
+    public void sumar(int a, int b, int c){
+        int resultado = a+b+c;
+        System.out.println(resultado);
+    }
+    
+}
